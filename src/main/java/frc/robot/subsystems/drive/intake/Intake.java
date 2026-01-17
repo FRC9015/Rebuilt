9637eef8b18943875b15ad6b666510e82f4f999a
Etienne-French <afialon1@jh.edu>
2026-01-17 11:10:52 -0500

    sorry, last nights commit didnt work, i added some framework for basic commands

--- src/main/java/frc/robot/subsystems/drive/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/drive/intake/Intake.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems.drive.intake;
 
-public class Intake {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
     
+
+    public Command extendIntake(){
+        return this.runOnce(this::extend);
+    }
+    /**
+     * intakes fuel, plz use toggle
+     */
+    public Command intakeFuel(){
+        return this.run(this::setIntakeMotorSpeeds);
+    }
+    private void setIntakeMotorSpeeds(){
+
+    }
+    private void extend(){
+
+    }
 }
