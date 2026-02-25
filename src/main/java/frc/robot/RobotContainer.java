4c9b27e4e7c82afe02945a5dbc325b630a414900
Etienne-French <afialon1@jh.edu>
2026-02-25 17:14:56 -0500

    made the code look good

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -269,13 +269,29 @@ public class RobotContainer {
 
     operatorController.y().onTrue(new TurretAngleAim(() -> drive.getPose(), turret));
 
-    operatorController.leftStick().onTrue(gamestate.manualGameData(0));
-    operatorController.rightStick().onTrue(gamestate.manualGameData(1));
 
-    operatorController.rightTrigger().whileTrue(shooter.runShooterAtSpeed(0.0).alongWith(indexer.runIndexerWithAutoUnjam(0)));
+    operatorController
+        .leftStick()
+        .onTrue(
+            gamestate.manualGameData(0));
+    operatorController
+        .rightStick()
+        .onTrue(
+            gamestate.manualGameData(1));
+    operatorController
+        .rightTrigger()
+        .whileTrue(
+            shooter.runShooterAtSpeed(0.0)
+            .alongWith(indexer.runIndexerWithAutoUnjam(0)));
     //operatorController.povRight().whileTrue(shooter.);
-    operatorController.b().whileTrue(shooter.setHoodPosition(0)); //10 degrees
-    operatorController.a().whileTrue(shooter.setHoodPosition(0)); //10 degrees
+    operatorController
+        .b()
+        .whileTrue(
+            shooter.setHoodPosition(0)); //10 degrees
+    operatorController
+        .a()
+        .whileTrue(
+            shooter.setHoodPosition(0)); //10 degrees
   }
 
   /**
