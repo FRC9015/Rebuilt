120811dfbc988e2445fdd1bb2fd0beb22476cc0f
Omkaar Sampigeadi <145941839+Omkaarcodes@users.noreply.github.com>
2026-02-28 12:39:23 -0500

    more changes (cleanup)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -221,27 +221,6 @@ public final class Constants {
     public static final String GAMEPIECE = "Fuel";
   }
 
-  public static class ClimbConstants {
-    public static final Slot0Configs climbSlot0Configs =
-        new Slot0Configs()
-            .withKP(0.1)
-            .withKI(0)
-            .withKD(0)
-            .withKG(0.01)
-            .withKA(0)
-            .withKS(0)
-            .withKV(0);
-
-    public static final FeedbackConfigs CLIMB_FEEDBACK_CONFIGS =
-        new FeedbackConfigs().withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor);
-
-    public static final MotionMagicConfigs CLIMB_MAGIC_CONFIGS =
-        new MotionMagicConfigs().withMotionMagicAcceleration(100).withMotionMagicCruiseVelocity(25);
-
-    public static final double CLIMB_MAX_POS = 300.0;
-    public static final double CLIMB_MIN_POS = 0.0;
-  }
-
   public static class ShooterConstants {
     //  Following naming scheme for subsystem motor and sensor ids
     public static final int FLY_WHEEL_LEFT_ID = 11;
