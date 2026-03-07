7868bbf145c831cf1ec06e2bca8e973dd13c34d4
Levinator123 <levisrobison@gmail.com>
2026-03-07 04:11:46 -0500

    finished tof interp

--- src/main/java/frc/robot/InterpTables.java
+++ src/main/java/frc/robot/InterpTables.java
@@ -13,18 +13,18 @@ public class InterpTables {
       new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Interpolator.forDouble());
 
   public InterpTables() {
-    timeOfFlightInterp.put(2.38, 0.0);
-    timeOfFlightInterp.put(1.68, 0.0);
-    timeOfFlightInterp.put(3.31, 0.0);
-    timeOfFlightInterp.put(4.14, 0.0);
-    timeOfFlightInterp.put(3.07, 0.0);
-    timeOfFlightInterp.put(3.16, 0.0);
-    timeOfFlightInterp.put(1.37, 0.0);
-    timeOfFlightInterp.put(2.78, 0.0);
-    timeOfFlightInterp.put(2.85, 0.0);
-    timeOfFlightInterp.put(0.0, 0.0);
-    timeOfFlightInterp.put(0.0, 0.0);
-    timeOfFlightInterp.put(0.0, 0.0);
+    timeOfFlightInterp.put(2.38, 0.8145);
+    timeOfFlightInterp.put(1.68, 0.9745);
+    timeOfFlightInterp.put(3.31, 0.8845);
+    timeOfFlightInterp.put(4.14, 1.0145);
+    timeOfFlightInterp.put(3.07, 1.2045);
+    timeOfFlightInterp.put(3.16, 1.1145);
+    timeOfFlightInterp.put(1.37, 1.1245);
+    timeOfFlightInterp.put(2.78, 1.3845);
+    timeOfFlightInterp.put(2.85, 1.4045);
+    timeOfFlightInterp.put(1.18, 1.1545);
+    timeOfFlightInterp.put(2.01, 0.7945);
+    timeOfFlightInterp.put(4.40, 1.1745);
     
     
     
