839ede16edcf3e21dcd40401c9ffe8862867cf5c
Omkaar Sampigeadi <145941839+Omkaarcodes@users.noreply.github.com>
2026-06-15 18:30:22 -0400

    test binbdings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -446,7 +446,7 @@ public class RobotContainer {
         .whileTrue(shooter.setKickerSpeedCommand(100).onlyIf(() -> DriverStation.isTest()));
     driverController
         .leftBumper()
-        .whileTrue(shooter.runShooterSpeed(23).onlyIf(() -> DriverStation.isTest()));
+        .whileTrue(shooter.setKickerSpeedCommand(-100).onlyIf(() -> DriverStation.isTest()));
     driverController
         .y()
         .whileTrue(
@@ -477,21 +477,18 @@ public class RobotContainer {
         .povRight()
         .whileTrue(indexer.runIndexer(-100).onlyIf(() -> DriverStation.isTest()));
 
-    // Intake forward (operator) and reverse (driver left trigger already maps to reverse)
     operatorController
         .leftTrigger()
         .whileTrue(intake.runRollerAtSpeed(100).onlyIf(() -> DriverStation.isTest()));
 
-    // Shooter manual forward/reverse for test mode
     operatorController
         .start()
-        .whileTrue(shooter.runShooterSpeed(100).onlyIf(() -> DriverStation.isTest()));
+        .whileTrue(shooter.runShooterSpeed(20).onlyIf(() -> DriverStation.isTest()));
     operatorController
         .back()
-        .whileTrue(shooter.runShooterSpeed(-100).onlyIf(() -> DriverStation.isTest()));
+        .whileTrue(shooter.runShooterSpeed(-20).onlyIf(() -> DriverStation.isTest()));
 
-    // Keep driver a-button mapping as an alternate control (reverse indexer)
-    driverController.a().whileTrue(indexer.runIndexer(-100));
+  
   }
 
   /**
